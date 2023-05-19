21a58f1c595ab3398222bfe463ee18fdf4e51271
Naim Ali <naimalinaim1@gmail.com>
2023-05-19 07:34:34 +0600

    404 error page added

--- src/pages/Shared/ErrorPage/ErrorPage.jsx
+++ src/pages/Shared/ErrorPage/ErrorPage.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+import errorImage from "../../../assets/error-img.jpg";
+
 const ErrorPage = () => {
-  return <div>opps</div>;
+  return (
+    <div className="h-screen flex flex-col justify-center items-center">
+      <img className="w-[550px]" src={errorImage} alt="" />
+      <button className="btn btn-error">
+        <Link to="/">Back to Home</Link>
+      </button>
+    </div>
+  );
 };
 
 export default ErrorPage;
